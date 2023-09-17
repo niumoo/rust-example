7b59e540a593118c29b200af48196d35e84b101c
niumoo <niumoo@126.com>
2023-09-17 15:48:19 +0800

    rust-struct-method

--- 12.rust-struct-method/src/main.rs
+++ 12.rust-struct-method/src/main.rs
@@ -6,6 +6,18 @@ fn main() {
     };
     re.update_width(10);
     println!("{} x {} = {}", re.width, re.height, re.area());
+
+    let re2 = Rectangle {
+        width: 8,
+        height: 5,
+    };
+    let re3 = Rectangle {
+        width: 80,
+        height: 50,
+    };
+    println!("{},{}", re.can_hold(&re2), re.can_hold(&re3));
+    let re4 = Rectangle::square(10);
+    println!("{} x {} = {}", re4.width, re4.height, re4.area());
 }
 
 // 结构体中的方法，
@@ -23,5 +35,23 @@ impl Rectangle {
 
     fn update_width(&mut self, w: u32) {
         self.width = w;
+    }
+}
+
+/**所有在 impl 块中定义的函数被称为 关联函数（associated functions），因为它们与 impl 后面命名的类型相关。我们可以定义不以 self 为第一参数的关联函数（因此不是方法），因为它们并不作用于一个结构体的实例。我们已经使用了一个这样的函数：在 String 类型上定义的 String::from 函数。
+ * 
+ */
+impl Rectangle {
+    // 传入另一个参数
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // 没有self 参数的关联函数
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
     }
 }
